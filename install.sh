6467abd08e8c0e08486876961ad6c20a5925dc76
Ian Walter <pub@ianwalter.dev>
2020-05-27 14:07:55 -0400

    Adding Linuxbrew install

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+platform=$(uname)
+
 if [[ ! `which brew` ]]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  if [[ $platform == 'Linux' ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 fi
 
 brew bundle
