d49f3cb01bc2f59ee2c8629a207ea07be933d4cc
Ian Walter <pub@ianwalter.dev>
2020-05-27 14:09:44 -0400

    Removing Linuxbrew install command since its merged into Homebrew

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-platform=$(uname)
-
 if [[ ! `which brew` ]]; then
-  if [[ $platform == 'Linux' ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-  else
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  fi
+  # Install Homebrew (and Linuxbrew).
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew bundle
